f1076294fa5c4e893252cd6fd835cd8b96adf3c8
LucasGallant <lucasbgallantjr@gmail.com>
2026-09-15 13:19:06 -0400

    Added json saving (kinda not really)

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import json
 
 nameXPATH = "/html/body/div[2]/div/div/section[2]/section/div[2]/div[2]/div[1]/h1"
 priceXPATH = "/html/body/div[2]/div/div/section[2]/section/div[2]/div[2]/section[3]/div/div[1]/div[2]/section/div/table/tr[1]/td[2]"
@@ -25,8 +26,16 @@ def scrapeCardUrl(cardUrl):
         print("Not Found")
         driver.quit()
     else:
-        print("Found")
-        print("Card Name: ", name.text)
-        print("Card Price: ", price.text)
-        print("Avg. Daily Sold: ", avgDailySold.text)
+        jFile = {
+            "name": name.text,
+            "price": price.text,
+            "avgDailySold": avgDailySold.text
+        }
+        json_str = json.dumps(jFile, indent=3)
+        with open("sample.json", "w") as f:         
+            f.write(json_str)
+        # print("Found")
+        # print("Card Name: ", name.text)
+        # print("Card Price: ", price.text)
+        # print("Avg. Daily Sold: ", avgDailySold.text)
         driver.quit()
