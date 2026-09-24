ed6aeff3ed6a44ba6bb3feb0e7a307691109eab6
LucasGallant <lucasbgallantjr@gmail.com>
2026-09-24 13:45:31 -0400

    working on json garbage

--- scrapeCards.py
+++ scrapeCards.py
@@ -3,7 +3,7 @@ from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
 import json
 
 nameXPATH = "/html/body/div[2]/div/div/section[2]/section/div[2]/div[2]/div[1]/h1"
@@ -21,37 +21,36 @@ def scrapeCardUrl(url):
 
     try: 
         # have to use WebDriverWait or else it will return null
-        #Maybe Faster?
         name = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, nameXPATH)))
-        # name = driver.find_element(By.XPATH, nameXPATH)
         price = driver.find_element(By.XPATH, priceXPATH)
         avgDailySold = driver.find_element(By.XPATH, avgDailySoldXPATH)
         setName = driver.find_element(By.XPATH, setNameXPATH)
-        
+
     # exception if no element was found for any of them
-    #NEEDS TO BE CHANGED TO CHECK INDIVIDUAL
+    # NEEDS TO BE CHANGED TO CHECK INDIVIDUAL
     except NoSuchElementException:
         print("Not Found")
         driver.quit()
     else:
-        # jFile = {
-        #     "name": name.text,
-        #     "cardInfo": {
-        #                 "price": price.text,
-        #                 "avgDailySold": avgDailySold.text
-        #                 }
-        # }
-        # json_str = json.dumps(jFile, indent=3)
-        # with open("sample.json", "w") as f:         
-        #     f.write(json_str)
-        if not (name == None or price == None or avgDailySold == None or setName == None):
-            print("--------------------------------------------------------------------------------------")
-            print("Card Name: ", name.text)
-            print("Card Price: ", price.text)
-            print("Avg. Daily Sold: ", avgDailySold.text)
-            print("Set: ", setName.text)
-            print("Url: ", url)
-        else:
-            print("Null Value")
+        document = Path.home()
+        filePath = document
+        jFile = {
+            "name": name.text,
+            "price": price.text,
+            "avgDailySold": avgDailySold.text,
+            "set": setName.text,
+            "url": url
+        }
+        with open(filePath, "w") as f:         
+            json.dump(jFile, f)
+        # if not (name == None or price == None or avgDailySold == None or setName == None):
+        #     print("--------------------------------------------------------------------------------------")
+        #     print("Card Name: ", name.text)
+        #     print("Card Price: ", price.text)
+        #     print("Avg. Daily Sold: ", avgDailySold.text)
+        #     print("Set: ", setName.text)
+        #     print("Url: ", url)
+        # else:
+        #     print("Null Value")
 
         driver.quit()
